512b118cb6f2ca778770ddd2b7bd7facc8969750
Matt Morse <mmorse@comcast.net>
2014-05-12 14:39:20 -0600

    styled with SASS

---
+++ assets/JS/main.js
@@ -0,0 +1,21 @@
+$(function() {
+	
+
+	$('.pack-image').mouseenter(function(e){
+		e.preventDefault();
+		$(this).animate({
+			'marginTop' : '-=75px'
+		});
+	});
+	$('.pack-image').mouseleave(function(e){
+		e.preventDefault();
+		$(this).animate({
+			'marginTop' : '+=75px'
+		});
+	});
+
+
+
+		
+
+});
