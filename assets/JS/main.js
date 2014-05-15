848330b81c27e3ba511ed2285e816c440aa2831e
Matt Morse <mmorse@comcast.net>
2014-05-15 17:55:40 -0600

    smooter animation

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,36 +1,35 @@
 $(function() {
 	
 
-	$('.pack-image').mouseenter(function(){
-		$(this).animate({'marginTop' : '-=75px'})
+	// $('.pack-image').mouseenter(function(){
+	// 	$(this).animate({'marginTop' : '-=75px'})
 		
-	});
+	// });
 	
-	$('.pack-image').mouseleave(function(e){
-		$(this).animate({'marginTop' : '+=75px'});
-	});
+	// $('.pack-image').mouseleave(function(e){
+	// 	$(this).animate({'marginTop' : '+=75px'});
+	// });
 
 
-    // var original = [];
-    // $('.pack-image').each(function (i) {
-    //     original.push($(this).css('margin-top'));
-    // });
-    // $('.pack-image').hover(function (e) {
-    //     $(this).stop().animate(
-    //         {"margin-top" : (
-    //             $(this).parent().outerHeight() - 
-    //             $(this).outerHeight()
-    //         )},
-    //         300
-    //     );
-    // }, function (e){
-    //     var i = $('.pack-image').index($(this));
-    //     $(this).stop().animate(
-    //         {"margin-top": original[i]},
-    //         250
-    //     );
-    // });
-
+     var original = [];
+    $('.pack-image').each(function (i) {
+        original.push($(this).css('margin-top'));
+    });
+    $('.pack-image').hover(function (e) {
+        $(this).stop().animate(
+            {"margin-top" : (
+                $(this).parent().outerHeight() - 
+                $(this).outerHeight()
+            )},
+            250
+        );
+    }, function (e){
+        var i = $('.pack-image').index($(this));
+        $(this).stop().animate(
+            {"margin-top": original[i]},
+            250
+        );
+    });
 	
 		
 
