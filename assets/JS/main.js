7af92b445a97ea04a4c1ac1324c158112948aed6
Matt Morse <mmorse@comcast.net>
2014-05-15 16:23:14 -0600

    addtional animation

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -3,13 +3,17 @@ $(function() {
 
 	$('.pack-image').mouseenter(function(){
 		$(this).animate({'marginTop' : '-=75px'})
+		$(this).find('.bonus-details').slideDown(452);
 	});
 	
 	$('.pack-image').mouseleave(function(e){
 		$(this).animate({'marginTop' : '+=75px'});
+		$(this).find('.bonus-details').slideUp();
 	});
 
 
+
+	
 		
 
 });
