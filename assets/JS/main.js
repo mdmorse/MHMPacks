f69e33bf06d74aa0a3267406338a259f7515af8d
Matt Morse <mmorse@comcast.net>
2014-05-16 18:16:30 -0600

    nav arrow animation

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,6 +1,6 @@
 $(function() {
 	
-
+	//***************************slide up to reveal pack overview and action buttons in main body*****************************
      var original = [];
     $('.pack-image').each(function () {
         original.push($(this).css('margin-top'));
@@ -13,13 +13,28 @@ $(function() {
             )},
             250
         );
-    }, function (e){
+    }, function (){
         var i = $('.pack-image').index($(this));
         $(this).stop().animate(
             {"margin-top": original[i]},
             250
         );
     });
+    //*********************navigation arrows rotation animation and toggle class***************************
+   $('.arrow-down').click(function(){
+        $('.show-js').slideToggle('fast').toggleClass('showing');
+        if(!$('.show-js').hasClass('showing')){
+            TweenLite.to($(this), .20, {rotation:0});
+            $('nav a').css('color','#161717');
+            $(this).css('border-top', '5px solid #161717')
+        }else{
+            TweenLite.to($(this), .20, {rotation:180});
+            $(this).closest('nav a').css('color','#4C69A1');
+            $(this).css('border-top', '5px solid #4C69A1');
+        }
+    });
+  
+
 	
 		
 
