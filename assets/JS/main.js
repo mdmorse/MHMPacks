65721090b245c5000801f971eee5bd11a2a26ba4
Matt Morse <mmorse@comcast.net>
2014-05-19 17:43:07 -0600

    worked on logic with the animation arrows

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -22,13 +22,16 @@ $(function() {
     });
     //*********************navigation arrows rotation animation and toggle class***************************
    $('.arrow-down').click(function(){
+        $('.arrow-down').toggleClass('.original-position');
+        TweenLite.to($(':not(.original-position)'), .20, {rotation:0});
         $('.show-js').slideToggle('fast').toggleClass('showing');
         if(!$('.show-js').hasClass('showing')){
             TweenLite.to($(this), .20, {rotation:0});
             $('nav a').css('color','#161717');
-            $(this).css('border-top', '5px solid #161717')
+            $(this).css('border-top', '5px solid #161717');
         }else{
             TweenLite.to($(this), .20, {rotation:180});
+            TweenLite.to($('.showing').siblings(), .20, {rotation: 0});
             $(this).closest('nav a').css('color','#4C69A1');
             $(this).css('border-top', '5px solid #4C69A1');
         }
